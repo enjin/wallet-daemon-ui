5f6fe307dabab62876712560b859bc4cbec6dae4
Leonardo Custodio <leonardo@enjin.io>
2023-10-20 13:34:02 -0300

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,11 +33,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _enjinMatrixKey = new TextEditingController();
+  final TextEditingController _canaryMatrixKey = new TextEditingController();
+  final TextEditingController _walletPassword = new TextEditingController();
+
   String currentNetwork = 'enjin-matrix';
+  final platformKeys = {
+    'enjin-matrix': '',
+    'canary-matrix': '',
+  };
+
+  String walletPassword = '';
 
   List<String> output = [];
-  late Process daemon;
+  Process? daemon;
   String walletAddress = '';
+
+  void setConfig() {
+    final configs = {
+      'enjin-matrix': {
+        "node": "wss://rpc.matrix.blockchain.enjin.io:443",
+        "api": "https://platform.enjin.io/graphql",
+        "master_key": "store"
+      },
+      'canary-matrix': {
+        "node": "wss://rpc.matrix.canary.enjin.io:443",
+        "api": "https://platform.canary.enjin.io/graphql",
+        "master_key": "store"
+      }
+    };
+
+    final config = configs[currentNetwork];
+    final configJson = jsonEncode(config);
+    File('config.json').writeAsStringSync(configJson);
+  }
 
   void addToOutput(String otp) {
     if (walletAddress == '' && otp.contains('Enjin Matrix:')) {
@@ -61,11 +91,12 @@ class _MyHomePageState extends State<MyHomePage> {
       'wallet/linux/wallet',
       [],
       environment: {
-        "KEY_PASS": "testando",
+        "KEY_PASS": 'testando', //walletPassword,
+        "PLATFORM_KEY": platformKeys[currentNetwork] ?? '',
         "CONFIG_FILE": "config.json",
       },
     );
-    daemon.stdout.transform(utf8.decoder).forEach(addToOutput);
+    daemon!.stdout.transform(utf8.decoder).forEach(addToOutput);
   }
 
   void stopWallet() {
@@ -75,7 +106,9 @@ class _MyHomePageState extends State<MyHomePage> {
       walletAddress = '';
     });
 
-    daemon.kill();
+    if (daemon != null) {
+      daemon!.kill();
+    }
   }
 
   List<Widget> getText() {
@@ -92,6 +125,186 @@ class _MyHomePageState extends State<MyHomePage> {
       );
     }
     return widgets;
+  }
+
+  void setKeys() {
+    platformKeys['enjin-matrix'] = _enjinMatrixKey.text;
+    platformKeys['canary-matrix'] = _canaryMatrixKey.text;
+    walletPassword = _walletPassword.text;
+  }
+
+  // 7wlRtl54SDVwXfKsnOjmthTrGrOOHLe9K30mWDpT2bfc9efb
+
+  Future<void> _showConfigDialog() async {
+    _enjinMatrixKey.text = platformKeys['enjin-matrix'] ?? '';
+    _canaryMatrixKey.text = platformKeys['canary-matrix'] ?? '';
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Enjin Platform - Configuration'),
+            ],
+          ),
+          contentPadding:
+              const EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 10),
+          children: <Widget>[
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Enjin Matrixchain',
+              style: TextStyle(
+                color: Color(0xFF434A60),
+                fontSize: 12,
+                fontFamily: 'Hauora',
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: 360,
+              height: 48,
+              child: TextField(
+                controller: _enjinMatrixKey,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.only(left: 10, right: 10),
+                  hintText: 'Input your platform key',
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color(0xFF7567CE),
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Canary Matrixchain',
+              style: TextStyle(
+                color: Color(0xFF434A60),
+                fontSize: 12,
+                fontFamily: 'Hauora',
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: 360,
+              height: 48,
+              child: TextField(
+                controller: _canaryMatrixKey,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.only(left: 10, right: 10),
+                  hintText: 'Input your platform key',
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color(0xFF7567CE),
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Auto generated wallet password (read-only)',
+              style: TextStyle(
+                color: Color(0xFF434A60),
+                fontSize: 12,
+                fontFamily: 'Hauora',
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: 360,
+              height: 48,
+              child: TextField(
+                readOnly: true,
+                controller: _walletPassword,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color(0xFF7567CE),
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                  onPressed: () {
+                    setKeys();
+                    Navigator.pop(context);
+                  },
+                  height: 48,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  color: Color(0xFF7866D5),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Text(
+                    "Apply",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                MaterialButton(
+                  onPressed: () => Navigator.pop(context),
+                  height: 48,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  color: Color(0xFF7866D5),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Text(
+                    "Cancel",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (walletPassword == '') {
+      walletPassword = const Uuid().v4();
+      _walletPassword.text = walletPassword;
+    }
   }
 
   @override
@@ -124,6 +337,9 @@ class _MyHomePageState extends State<MyHomePage> {
                     initialSelection: currentNetwork,
                     onSelected: (String? value) {
                       currentNetwork = value!;
+                      print(currentNetwork);
+                      stopWallet();
+                      setConfig();
                     },
                     dropdownMenuEntries: [
                       DropdownMenuEntry<String>(
@@ -200,7 +416,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     width: 10,
                   ),
                   MaterialButton(
-                    onPressed: () {},
+                    onPressed: _showConfigDialog,
                     height: 48,
                     padding: const EdgeInsets.symmetric(
                         horizontal: 16, vertical: 12),
